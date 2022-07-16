ad41f6196cbbc219ad57054c6070ddaec11718a3
yang vieira <yangflix3@gmail.com>
2022-07-16 15:22:38 -0400

    teste de unidade

---
+++ greet/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace greet;
+
+public class Ola
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+    }
+    public static string Greet()
+    {
+        return "Olá";
+    }
+}
+
+
+
+
