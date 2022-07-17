9a3f67baad153b01ab18db7c673882dbb011fb68
yang vieira <yangflix3@gmail.com>
2022-07-17 12:36:13 -0400

    greet test

--- greet.Test/UnitTest1.cs
+++ greet.Test/UnitTest1.cs
@@ -5,11 +5,16 @@ namespace greet.Test;
 
 public class UnitTest1
 {
-[Theory(DisplayName = "Deve retornar \"Olá\"")]
-[InlineData("Olá")]
-public void TestGreet(string expected)
+[Theory(DisplayName = "deve retornar as somas dos numeros")]
+[InlineData(1,1,2)]
+[InlineData(10,10, 20)]
+[InlineData(50,80, 130)]
+public void TestGreet(int number, int number2, int expected)
 {
-    var result = Ola.Greet();
+    var result = Ola.Greet(number, number2);
     result.Should().Be(expected);
+
 }
+
+
 }
