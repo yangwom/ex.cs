50ad3111453eae59f1997b789ae94e23cfee304e
yang vieira <yangflix3@gmail.com>
2022-07-25 00:01:09 -0400

    :sparkles: bricando com condicionais

--- estruturas-de-controle/condicionaisC#/Program.cs
+++ estruturas-de-controle/condicionaisC#/Program.cs
@@ -1,32 +1,64 @@
 ﻿namespace condicionais;
 
 
- public class Program
+public class Program
 {
-public static int radius = 0;
-  public static void Main() 
-  {
-    Console.WriteLine("Informe o raio de um círculo (deve ser um número inteiro)");
-    string? response = Console.ReadLine();
-    /* tryParse recebe o primeiro parametro que será a string que tem um inteiro a ser convertido 
-    e segundo  vai retornar o inteiro convertido de response, se for passado um inteiro no 
-    parametro o metódo retornara true, caso ao contrario  false.
-    */
-    var canConvert = Int32.TryParse(response, out radius);
+    public static int radius;
 
-      if(canConvert)
+    public static int number;
+    public static void Main()
+    {
+        Console.WriteLine("Informe o raio de um círculo (deve ser um número inteiro)");
+        string response = Console.ReadLine()!;
+        /* int32.tryParse recebe o primeiro parametro que será a string que tem um inteiro a ser convertido 
+        e segundo  vai retornar o inteiro convertido de response, se for passado um inteiro no 
+        parametro o metódo retornara true, caso ao contrario  false.
+        */
+
+
+        var canConvert = int.TryParse(response, out radius);
+
+        if (canConvert)
         {
             const double pi = 3.14159;
 
             double circumference = pi * (2 * radius);
 
-            Console.WriteLine("A circunferência de um circulo com raio "+ radius + " é igual a " + circumference);
+            Console.WriteLine("A circunferência de um circulo com raio " + radius + " é igual a " + circumference);
         }
         else
         {
             Console.WriteLine("O texto digitado não é um número inteiro.");
         }
-    }            
-    
-  }
+        
+        VerifyNumber();
+
+    }
+    public static void VerifyNumber()
+
+    {
+        Console.WriteLine("digite um numero");
+
+        int isNumber = int.Parse(Console.ReadLine()!);
+
+        if (isNumber > 0)
+        {
+            Console.WriteLine("maior que zero");
+        }
+        else
+        {
+            if (isNumber < 0)
+
+            {
+                Console.WriteLine("menor que zero");
+            }
+            else
+            {
+                Console.WriteLine("igual a zero");
+            }
+        }
+    }
+}
+
+
 
